10f16e083df1549614e7d2ae34e0757f5eacad00
chenxiaolani <2524729719@qq.com>
2020-10-24 22:52:26 +0800

    添加部署命令同步部署到gihub和gitee

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist
+yarn build
+cd dist
+git init
+git add .
+git commit -m "update"
+git remote add origin git@github.com:mywebc/le-vue.git
+git remote add gitee git@gitee.com:ChenXiaoLaNi/le-ui-docs.git
+git push -f -u origin master:docs
+git push -f -u gitee master
+cd -
