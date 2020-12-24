30c04f3823c49e8ff3f3e763940db41e569022ab
chenxiaolani <2524729719@qq.com>
2020-12-24 15:27:18 +0800

    组件导入的补充

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,4 +4,15 @@ export {default as Table } from "./Table.vue";
 export {default as Tabs } from "./Tabs.vue";
 export {default as Tab } from "./Tab.vue";
 export {default as Dialog } from "./Dialog.vue";
+export {default as Icon } from "./Icon.vue";
+export {default as Input} from "./Input.vue";
+export {default as Rate} from "./Rate.vue";
+export {Content, Footer, Header, Layout, Side} from "./Layout";
+export {default as Spin} from "./Spin.vue";
+export {default as Radio} from "./Radio/Radio.vue";
+export {default as RadioGroup} from "./Radio/RadioGroup.vue";
+export {default as CheckBox} from "./CheckBox/CheckBox.vue";
+export {default as CheckBoxGroup} from "./CheckBox/CheckBoxGroup.vue";
+
+
 
